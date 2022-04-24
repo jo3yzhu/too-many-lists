c772a50d9656a0f23e4d5b7df70035159cbcd8bd
jo3yzhu <jo3yzhu@gmail.com>
2022-04-24 16:15:56 +0800

    add operation and test of persistent list

--- src/third.rs
+++ src/third.rs
@@ -16,14 +16,14 @@ impl<T> List<T> {
         List { head: None }
     }
 
-    pub fn perpend(&mut self, val: T) -> List<T> {
+    pub fn prepend(&mut self, val: T) -> List<T> {
         List::<T> {
             head: {
                 Some(Rc::new(Node {
                     val,
                     // Option<T> has implementated Copy traits, whose behavior is:
                     // Some(x) => Some(x.clone())
-                    // None => None()
+                    // None => None
                     // so clone() here calling clone() of Rc<T>
                     next: self.head.clone(),
                 }))
@@ -31,14 +31,21 @@ impl<T> List<T> {
         }
     }
 
-    pub fn tail(&mut self) -> Option<List<T>> {
-        todo!()
+    pub fn tail(&mut self) -> List<T> {
+        List {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.val)
     }
 }
 
 #[cfg(test)]
 mod tests {
 
+    use super::List;
     use std::rc::Rc;
 
     #[test]
@@ -54,5 +61,51 @@ mod tests {
         let b = Some(Box::new(1));
         let copied_b = b.clone();
         assert_eq!(b.unwrap(), copied_b.unwrap());
+    }
+
+    #[test]
+    fn basic_test() {
+        
+
+        let mut list = List::<i32>::new();
+        assert_eq!(list.head(), None);
+    
+        // 3 -> 2 -> 1
+        let mut list = list.prepend(1).prepend(2).prepend(3);
+        assert_eq!(list.head(), Some(&3));
+
+        
+        // 3
+        // |
+        // v
+        // 2 -> 1
+        let mut list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        //      3
+        //      |
+        //      v
+        // 4 -> 2 -> 1
+        let mut list = list.prepend(4);
+        assert_eq!(list.head(), Some(&4));
+
+        // 3
+        // |
+        // v
+        // 2 -> 1
+        let mut list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        // 1
+        let mut list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+        
+        // []
+        let mut list = list.tail();
+        assert_eq!(list.head(), None);
+
+        // []
+        let list = list.tail();
+        assert_eq!(list.head(), None);
     }
 }
